f1ae8f0a5ca9c98206954fcfd18b8e80b64c741f
farahyasserr <farah.dawoudd@gmail.com>
2023-02-03 21:46:11 +0400

    Adding icons to bottom tab bar

--- src/navigation/BottomTab.tsx
+++ src/navigation/BottomTab.tsx
@@ -4,20 +4,44 @@ import HomeScreen from '../screens/HomeScreen/HomeScreen.view';
 import ProfileScreen from '../screens/ProfileScreen/ProfileScreen.view';
 import TimeScreen from '../screens/TimeScreen/TimeScreen.view';
 import Header from '../ui/components/Header/Header.view';
+import {Image} from 'react-native';
+import images from '../assets/images';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTab = () => {
   const headerTitle = () => <Header />;
+  const profileScreenOptions = {
+    tabBarIcon: () => <Image source={images.tabIcons.profileIcon} />,
+  };
+  const homeScreenOptions = {
+    tabBarIcon: () => <Image source={images.tabIcons.homeIcon} />,
+  };
+  const timeScreenOptions = {
+    tabBarIcon: () => <Image source={images.tabIcons.timeIcon} />,
+  };
 
   return (
     <Tab.Navigator
       screenOptions={{
         headerTitle: headerTitle,
+        tabBarShowLabel: false,
       }}>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
-      <Tab.Screen name="TimeScreen" component={TimeScreen} />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={homeScreenOptions}
+      />
+      <Tab.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={profileScreenOptions}
+      />
+      <Tab.Screen
+        name="TimeScreen"
+        component={TimeScreen}
+        options={timeScreenOptions}
+      />
     </Tab.Navigator>
   );
 };
--- src/ui/theme/colors.ts
+++ src/ui/theme/colors.ts
@@ -1,7 +1,9 @@
 const colors = {
   white: 'white',
+  black: 'black',
   green: '#65C54D',
   red: '#FF5F5F',
+  grey: 'grey',
 };
 
 export default colors;
