d253a55d1f2412bd07c093710f30cafa6daae09e
farahyasserr <farah.dawoudd@gmail.com>
2023-02-28 11:48:18 +0400

    fix: removing unused import

--- src/screens/HomeScreen/HomeScreen.view.tsx
+++ src/screens/HomeScreen/HomeScreen.view.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import CardsStack from './blocks/CardsStack/CardsStack.view';
 import styles from './HomeScreen.styles';
 
