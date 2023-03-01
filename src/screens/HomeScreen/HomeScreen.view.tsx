63a22f428ec43e45b57cbac0d8b607fe5e03a785
farahyasserr <farah.dawoudd@gmail.com>
2023-03-01 13:41:21 +0400

    fix!: testing whether fix will bump

--- src/screens/HomeScreen/HomeScreen.view.tsx
+++ src/screens/HomeScreen/HomeScreen.view.tsx
@@ -4,7 +4,7 @@ import CardsStack from './blocks/CardsStack/CardsStack.view';
 import styles from './HomeScreen.styles';
 
 const HomeScreen = (): JSX.Element => {
-  let x = 'testing the ! againnnnn';
+  let x = 'testing the !kk';
   return (
     <View style={styles.mainContainer}>
       <CardsStack />
