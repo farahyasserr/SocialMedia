b4f1a4caa3909ff70b0fe922f7456c76123afa91
farahyasserr <farah.dawoudd@gmail.com>
2023-03-01 13:06:09 +0400

    feat(test): test description
    BREAKING CHANGE: test
    
    n

--- src/screens/HomeScreen/HomeScreen.view.tsx
+++ src/screens/HomeScreen/HomeScreen.view.tsx
@@ -4,7 +4,7 @@ import CardsStack from './blocks/CardsStack/CardsStack.view';
 import styles from './HomeScreen.styles';
 
 const HomeScreen = (): JSX.Element => {
-  let x = 'BREAKING CHANGE / MAJOR testing bump';
+  let x = 'BREAKING CHANGE / MAJOR testing no bump';
   return (
     <View style={styles.mainContainer}>
       <CardsStack />
