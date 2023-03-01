c35a581594166c958d15e48cb1381c4706aae294
farahyasserr <farah.dawoudd@gmail.com>
2023-03-01 13:07:41 +0400

    feat(last test): test
    n

--- src/screens/HomeScreen/HomeScreen.view.tsx
+++ src/screens/HomeScreen/HomeScreen.view.tsx
@@ -4,7 +4,7 @@ import CardsStack from './blocks/CardsStack/CardsStack.view';
 import styles from './HomeScreen.styles';
 
 const HomeScreen = (): JSX.Element => {
-  let x = 'BREAKING CHANGE / MAJOR testing no bump';
+  let x = 'last test';
   return (
     <View style={styles.mainContainer}>
       <CardsStack />
