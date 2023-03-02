ca4ebea763cf0e0991a686fd96db816d5da91d43
farahyasserr <farah.dawoudd@gmail.com>
2023-03-02 10:04:35 +0400

    Normal commit

--- src/screens/HomeScreen/HomeScreen.view.tsx
+++ src/screens/HomeScreen/HomeScreen.view.tsx
@@ -4,7 +4,7 @@ import CardsStack from './blocks/CardsStack/CardsStack.view';
 import styles from './HomeScreen.styles';
 
 const HomeScreen = (): JSX.Element => {
-  let y = 11;
+  let y = 13;
   return (
     <View style={styles.mainContainer}>
       <CardsStack />
